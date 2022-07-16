521ef84234752248148d6aac9d31a5278d5ab433
mbo-ya <emonsarigo@gmail.com>
2022-07-17 01:21:02 +0300

    First project is madlibs.

---
+++ madlibs.py
@@ -0,0 +1,34 @@
+# 2022/07/05 22:48:24 -- madlibs.py
+# following KylieYing's youtube video
+# suppose we want to create a string that says "subscribe to _______"
+#youtuber = "inigo montoya" # some string variable
+#animal = "person bull"
+#
+#print(f"subscribe to {youtuber}. He is a {animal}")
+
+#adj = input("Adjective: ")
+#verb1 = input("Verb: ")
+#verb2 = input("Verb: ")
+#famous_person = input("Famous person: ")
+#
+#madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+#
+#print(madlib)
+
+noun1 = input("Noun: ")
+conjunction1 = input("Conjuction: ")
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun2 = input("Noun: ")
+adj3 = input("Adjective: ")
+pronoun1 = input("Pronoun: ")
+noun3 = input("Noun: ")
+noun4 = input("Noun: ")
+noun5 = input("Noun: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+noun6 = input("Noun: ")
+
+madlib = f"Each {noun1}, Westley struggled to wake up {conjunction1} he knew it was just another {adj1} that Buttercup would not {adj2} him. He longed to tell her {noun2} his love for her. But Buttercup was {adj3} that she didn't notice {pronoun1} at all. In other news, Inigo's {noun3} has just {noun4} and now he has to go train. He leaves {noun5} and goes to {verb1} for a teacher to {verb2} him to beat the six-fingered {noun6} in mortal duel."
+
+print(madlib)
