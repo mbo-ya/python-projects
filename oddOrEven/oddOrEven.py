200ff044e9666ecef7fe70b0dcac0fc16d81ba13
mbo-ya <emonsarigo@gmail.com>
2022-07-21 23:21:47 +0300

    Replace remainder (rem) variable with boolean expression in the conditional.

--- oddOrEven/oddOrEven.py
+++ oddOrEven/oddOrEven.py
@@ -2,9 +2,7 @@
 # on whether the number is odd or even.
 num = int(input("What number are you thinking? (1-1000): "))
 
-rem = num % 2
-
-if rem == 0:
+if (num % 2) == 0:
     print("That's an even number! Have another?")
 else:
     print("That's an odd number! Have another?")
